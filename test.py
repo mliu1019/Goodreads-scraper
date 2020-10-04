43e06df37ceace491c80b1e333675afa77569613
Shunfan Shao <minerl2@illinois.edu>
2020-10-04 17:56:31 -0500

    add gitlab-ci

--- test.py
+++ test.py
@@ -4,10 +4,6 @@ class Testing(unittest.TestCase):
     def test_databaseconnection(self):
         import pymongo as pm
         from database import mongoclient
-        import os
-
-        from dotenv import load_dotenv
-        load_dotenv("config.env")
 
         raised = False
         try:
@@ -18,6 +14,13 @@ class Testing(unittest.TestCase):
             raised = True
 
         self.assertFalse(raised, "Shoud not raise.")
+
+    def test_scrapebook(self):
+        from book import create_book
+        book = create_book("https://www.goodreads.com/book/show/3735293-clean-code")
+        self.assertEqual(book.book_url, "https://www.goodreads.com/book/show/3735293-clean-code")
+        self.assertEqual(book.author, "Robert C. Martin")
+        self.assertEqual(book.title, "Clean Code: A Handbook of Agile Software Craftsmanship")
 
 
 if __name__ == '__main__':
