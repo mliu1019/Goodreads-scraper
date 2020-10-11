0bc687242462c0a8bb558fa3ce45621b5a04f251
Miner Liu <minerl2@illinois.edu>
2020-10-11 17:07:34 -0500

    integrate gitlab ci to deploy heroku application

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
-git status
-git push heroku assignment-2.1:master
+export $(cat config.env | xargs) 
+gitlab-runner exec shell $1 \
+    --env MONGODB_DB=$MONGODB_DB \
+    --env MONGODB_USER=$MONGODB_USER \
+    --env MONGODB_PSWD=$MONGODB_PSWD \
+    --env HEROKU_API_KEY=$HEROKU_API_KEY
